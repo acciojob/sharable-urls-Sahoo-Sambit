aa5b890bb6e6b3109e3b4081966c0fab1b425aa5
Sambit_Sahoo <93509732+Sahoo-Sambit@users.noreply.github.com>
2023-03-15 23:00:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // your code here
+const form = document.querySelector('form');
+const urlElement = document.querySelector('#url');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // prevent the form from submitting normally
+
+  const name = document.querySelector('#name').value;
+  const year = document.querySelector('#year').value;
+
+  // build the query string
+  const queryString = `?name=${encodeURIComponent(name)}&year=${encodeURIComponent(year)}`;
+
+  // update the URL
+  urlElement.textContent = `https://localhost:8080/${queryString}`;
+});
